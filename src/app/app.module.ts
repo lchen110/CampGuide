c67327c8cb11a49d2105a86dbc5fe79a1147f60a
Lin Chen <linchen00314@gmail.com>
2017-07-03 17:22:32 -0400

    Initial commit -- has my-release-key.jks from firebase data (Free to make changes on that)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,23 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { GooglePlus } from '@ionic-native/google-plus';
+import { AngularFireModule } from 'angularfire2';
+// import  Firebase from 'firebase';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig={
+  apiKey: "AIzaSyAZXc6x-GMDCpY2qCCCj2r52o1vNMTSZv4",
+    authDomain: "campguide-1219e.firebaseapp.com",
+    databaseURL: "https://campguide-1219e.firebaseio.com",
+    projectId: "campguide-1219e",
+    storageBucket: "campguide-1219e.appspot.com",
+    messagingSenderId: "289231772988"
+}
+
+// firebase.initializeApp(firebaseConfig)
 
 @NgModule({
   declarations: [
@@ -14,7 +28,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +39,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    GooglePlus
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GooglePlus } from '@ionic-native/google-plus';
+/*import { AngularFireModule } from 'angularfire2';*/
+// import  Firebase from 'firebase';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public googleplus:GooglePlus) {
 
   }
 
+  // login(){
+  // 	// this.googleplus.login({
+  // 	// 	'webClientId':'289231772988-4rtvh91ivbqnpldba5trcgtm6ec40cql.apps.googleusercontent.com',
+  // 	// 	'offline':true
+  // 	// }).then(res=>{
+  // 	// 	firebase.auth.signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken))
+  // 	// 	.then(suc=>{
+  // 	// 		alert("LOGIN SUC")
+  // 	// 	}).catch(ns=>{
+  // 	// 		alert("NOT SUCC")
+  // 	// 	})
+  // 	// })
+  // }
 }
